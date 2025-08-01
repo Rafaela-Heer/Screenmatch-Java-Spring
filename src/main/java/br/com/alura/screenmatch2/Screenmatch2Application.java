7c544ddc6549b3e0795b0ff22819ee9bbacb5bf6
Rafaela-Heer <rafaelaheer@hotmail.com>
2025-08-01 11:05:21 -0300

    update obterDados

--- src/main/java/br/com/alura/screenmatch2/Screenmatch2Application.java
+++ src/main/java/br/com/alura/screenmatch2/Screenmatch2Application.java
@@ -1,5 +1,8 @@
 package br.com.alura.screenmatch2;
 
+import br.com.alura.screenmatch2.model.DadosSerie;
+import br.com.alura.screenmatch2.service.ConsumoApi;
+import br.com.alura.screenmatch2.service.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,6 +16,11 @@ public class Screenmatch2Application implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+		var consumoapi = new ConsumoApi();
+		var json = consumoapi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=162909ca");
+		System.out.println(json);
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
 	}
 }
